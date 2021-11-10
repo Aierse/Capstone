666d872998e764ad55b7227d481ef43985f0f093
Aierse <dbsgur2008@gmail.com>
2021-11-10 14:54:59 +0900

    탭 호스트 적용

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.capstone;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TabHost;
+import android.widget.TextView;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = findViewById(R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpecColor = tabHost.newTabSpec("COLOR").setIndicator("색상");
+        tabSpecColor.setContent(R.id.colorLayout);
+        tabHost.addTab(tabSpecColor);
+
+        TabHost.TabSpec tabSpecRota = tabHost.newTabSpec("ROTATION").setIndicator("회전");
+        tabSpecRota.setContent(R.id.rotationLayout);
+        tabHost.addTab(tabSpecRota);
+
+        tabHost.setCurrentTab(0);
     }
 }
