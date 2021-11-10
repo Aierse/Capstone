d4c289be28bc6ac2e28ff43377520ba63631acad
Aierse <dbsgur2008@gmail.com>
2021-11-10 15:54:57 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.capstone;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
-// test
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
